e00e8eb91664195ac1fc75cc2c64f011ef37aef7
BredexBR <breno.ferreira.bre2@gmail.com>
2025-01-20 15:28:18 -0300

    Feat: Tratamento de Excecoes ControllerAdvice, ResponseStatus e ExceptionHandle

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/MathController.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/MathController.java
@@ -5,23 +5,70 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.erudio.exceptions.UnsupMathOpException;
+
 @RestController
 public class MathController {
     
     @RequestMapping(value="/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
-    public Double sum(@PathVariable("numberOne") String numberOne,
-                      @PathVariable("numberTwo") String numberTwo) throws Exception {
+    public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
         if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-            throw new Exception();
+            throw new UnsupMathOpException("Please set a numeric value!");
         }
-        return covertToDouble(numberOne) + covertToDouble(numberTwo);
-    }    
+        Double result = covertToDouble(numberOne) + covertToDouble(numberTwo);
+        return result;        
+    }
+    
+    @RequestMapping(value="/subtraction/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Double subtraction(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupMathOpException("Please set a numeric value!");
+        }
+        Double result = covertToDouble(numberOne) - covertToDouble(numberTwo);
+        return result;        
+    }
+    
+    @RequestMapping(value="/multiplication/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Double multiplication(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupMathOpException("Please set a numeric value!");
+        }
+        Double result = covertToDouble(numberOne) * covertToDouble(numberTwo);
+        return result;        
+    }
+    
+    @RequestMapping(value="/division/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Double division(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupMathOpException("Please set a numeric value!");
+        }
+        Double result = covertToDouble(numberOne) / covertToDouble(numberTwo);
+        return result;        
+    }
+    
+    @RequestMapping(value="/mean/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Double mean(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupMathOpException("Please set a numeric value!");
+        }
+        Double result = (covertToDouble(numberOne) + covertToDouble(numberTwo)) / 2;
+        return result;        
+    }
+    
+    @RequestMapping(value="/squareRoot/{number}", method=RequestMethod.GET)
+    public Double squareRoot(@PathVariable("number") String number) throws Exception {
+        if (!isNumeric(number)) {
+            throw new UnsupMathOpException("Please set a numeric value!");
+        }
+        Double result = Math.sqrt(covertToDouble(number));
+        return result;        
+    }
     
     public static Double covertToDouble(String strNumber) {
         if (strNumber == null) return 0d; 
         String number = strNumber.replaceAll(",", ".");
         if (isNumeric(number)) return Double.parseDouble(number);
-        return 1.0d;
+        return 0d;
     }
 
     public static boolean isNumeric(String strNumber) {
