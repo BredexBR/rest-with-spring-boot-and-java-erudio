d6d7a11d0493320a26fb4441b46b13a1f4a50f04
BredexBR <breno.ferreira.bre2@gmail.com>
2025-01-23 16:41:19 -0300

    Feat: Adicionando custom json serialization e preparando pacote 13 para proxima aula

--- 13_ContentNegotiation/rest-with-spring-boot-and-java-erudio13/src/main/java/br/com/erudio/model/Person.java
+++ 13_ContentNegotiation/rest-with-spring-boot-and-java-erudio13/src/main/java/br/com/erudio/model/Person.java
@@ -1,18 +1,37 @@
-package br.com.erudio.data.vo.v1;
+package br.com.erudio.model;
 
 import java.io.Serializable;
 
-public class PersonVO implements Serializable {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "person")
+public class Person implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "first_name", nullable = false, length = 80)
 	private String firstName;
+
+	@Column(name = "last_name", nullable = false, length = 80)
 	private String lastName;
+	
+	@Column(nullable = false, length = 100)
 	private String address;
+	
+	@Column(nullable = false, length = 6)
 	private String gender;
 	
-	public PersonVO() {}
+	public Person() {}
 
 	public Long getId() {
 		return id;
@@ -74,7 +93,7 @@ public class PersonVO implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		PersonVO other = (PersonVO) obj;
+		Person other = (Person) obj;
 		if (address == null) {
 			if (other.address != null)
 				return false;
