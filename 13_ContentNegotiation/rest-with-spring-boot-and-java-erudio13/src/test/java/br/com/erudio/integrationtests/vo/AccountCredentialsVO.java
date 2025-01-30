002e772066186f2c2d07d21966ca0632a33c271c
BredexBR <breno.ferreira.bre2@gmail.com>
2025-01-30 14:53:21 -0300

    Feat: Criando testes de controller com requisicoes com formatos xml e yaml para Authorization, Books e Person.

--- 13_ContentNegotiation/rest-with-spring-boot-and-java-erudio13/src/test/java/br/com/erudio/integrationtests/vo/AccountCredentialsVO.java
+++ 13_ContentNegotiation/rest-with-spring-boot-and-java-erudio13/src/test/java/br/com/erudio/integrationtests/vo/AccountCredentialsVO.java
@@ -2,6 +2,9 @@ package br.com.erudio.integrationtests.vo;
 
 import java.io.Serializable;
 
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class AccountCredentialsVO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
